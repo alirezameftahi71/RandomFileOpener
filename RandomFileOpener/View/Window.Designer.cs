956357976d9d1ede3a2e57730baabdff0542c4c3
alirezameftahi71 <alirezameftahi_71@yahoo.com>
2019-04-19 23:47:48 +0430

    Created UI

--- RandomFileOpener/View/Window.Designer.cs
+++ RandomFileOpener/View/Window.Designer.cs
@@ -28,73 +28,197 @@ namespace RandomFileOpener
         /// </summary>
         private void InitializeComponent()
         {
-            this.PathLbl = new System.Windows.Forms.Label();
-            this.RandomBtn = new System.Windows.Forms.Button();
-            this.HeaderLbl = new System.Windows.Forms.Label();
+            this.ParentSplitContainer = new System.Windows.Forms.SplitContainer();
+            this.FindTargetBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.ReOpenBtn = new System.Windows.Forms.Button();
             this.BrowseBtn = new System.Windows.Forms.Button();
+            this.HeaderLbl = new System.Windows.Forms.Label();
+            this.RandomBtn = new System.Windows.Forms.Button();
+            this.LogLbl = new System.Windows.Forms.Label();
+            this.ActionTableCnt = new System.Windows.Forms.TableLayoutPanel();
+            this.BrowseTableCnt = new System.Windows.Forms.TableLayoutPanel();
+            this.PathLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.ParentSplitContainer)).BeginInit();
+            this.ParentSplitContainer.Panel1.SuspendLayout();
+            this.ParentSplitContainer.Panel2.SuspendLayout();
+            this.ParentSplitContainer.SuspendLayout();
+            this.ActionTableCnt.SuspendLayout();
+            this.BrowseTableCnt.SuspendLayout();
             this.SuspendLayout();
             // 
-            // PathLbl
+            // ParentSplitContainer
             // 
-            this.PathLbl.Location = new System.Drawing.Point(148, 64);
-            this.PathLbl.Name = "PathLbl";
-            this.PathLbl.Size = new System.Drawing.Size(551, 69);
-            this.PathLbl.TabIndex = 1;
-            this.PathLbl.Text = "Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app!";
+            this.ParentSplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ParentSplitContainer.Location = new System.Drawing.Point(0, 0);
+            this.ParentSplitContainer.Name = "ParentSplitContainer";
             // 
-            // RandomBtn
+            // ParentSplitContainer.Panel1
             // 
-            this.RandomBtn.Location = new System.Drawing.Point(12, 137);
-            this.RandomBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.RandomBtn.Name = "RandomBtn";
-            this.RandomBtn.Size = new System.Drawing.Size(130, 34);
-            this.RandomBtn.TabIndex = 2;
-            this.RandomBtn.Text = "Random!";
-            this.RandomBtn.UseVisualStyleBackColor = true;
-            this.RandomBtn.Click += new System.EventHandler(this.Button1_Click);
+            this.ParentSplitContainer.Panel1.Controls.Add(this.BrowseTableCnt);
+            this.ParentSplitContainer.Panel1.Controls.Add(this.ActionTableCnt);
+            this.ParentSplitContainer.Panel1.Controls.Add(this.HeaderLbl);
             // 
-            // HeaderLbl
+            // ParentSplitContainer.Panel2
             // 
-            this.HeaderLbl.AutoSize = true;
-            this.HeaderLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.HeaderLbl.Location = new System.Drawing.Point(180, 9);
-            this.HeaderLbl.Name = "HeaderLbl";
-            this.HeaderLbl.Size = new System.Drawing.Size(371, 31);
-            this.HeaderLbl.TabIndex = 3;
-            this.HeaderLbl.Text = "Pick An Address and Hit Run!";
+            this.ParentSplitContainer.Panel2.Controls.Add(this.LogLbl);
+            this.ParentSplitContainer.Size = new System.Drawing.Size(1016, 487);
+            this.ParentSplitContainer.SplitterDistance = 558;
+            this.ParentSplitContainer.TabIndex = 0;
+            // 
+            // FindTargetBtn
+            // 
+            this.FindTargetBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.FindTargetBtn.Location = new System.Drawing.Point(3, 104);
+            this.FindTargetBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.FindTargetBtn.Name = "FindTargetBtn";
+            this.FindTargetBtn.Size = new System.Drawing.Size(273, 99);
+            this.FindTargetBtn.TabIndex = 17;
+            this.FindTargetBtn.Text = "Show In Explorer";
+            this.FindTargetBtn.UseVisualStyleBackColor = true;
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DeleteBtn.Location = new System.Drawing.Point(282, 2);
+            this.DeleteBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(273, 98);
+            this.DeleteBtn.TabIndex = 16;
+            this.DeleteBtn.Text = "Delete";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            // 
+            // ReOpenBtn
+            // 
+            this.ReOpenBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ReOpenBtn.Location = new System.Drawing.Point(282, 104);
+            this.ReOpenBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ReOpenBtn.Name = "ReOpenBtn";
+            this.ReOpenBtn.Size = new System.Drawing.Size(273, 99);
+            this.ReOpenBtn.TabIndex = 15;
+            this.ReOpenBtn.Text = "ReOpen";
+            this.ReOpenBtn.UseVisualStyleBackColor = true;
             // 
             // BrowseBtn
             // 
-            this.BrowseBtn.Location = new System.Drawing.Point(12, 55);
+            this.BrowseBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowseBtn.Location = new System.Drawing.Point(3, 75);
             this.BrowseBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BrowseBtn.Name = "BrowseBtn";
-            this.BrowseBtn.Size = new System.Drawing.Size(130, 34);
-            this.BrowseBtn.TabIndex = 4;
+            this.BrowseBtn.Size = new System.Drawing.Size(552, 70);
+            this.BrowseBtn.TabIndex = 14;
             this.BrowseBtn.Text = "Browse";
             this.BrowseBtn.UseVisualStyleBackColor = true;
+            // 
+            // HeaderLbl
+            // 
+            this.HeaderLbl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.HeaderLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeaderLbl.Location = new System.Drawing.Point(0, 0);
+            this.HeaderLbl.Name = "HeaderLbl";
+            this.HeaderLbl.Size = new System.Drawing.Size(558, 52);
+            this.HeaderLbl.TabIndex = 13;
+            this.HeaderLbl.Text = "Random File Opener";
+            this.HeaderLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // RandomBtn
+            // 
+            this.RandomBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.RandomBtn.Location = new System.Drawing.Point(3, 2);
+            this.RandomBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.RandomBtn.Name = "RandomBtn";
+            this.RandomBtn.Size = new System.Drawing.Size(273, 98);
+            this.RandomBtn.TabIndex = 12;
+            this.RandomBtn.Text = "Random!";
+            this.RandomBtn.UseVisualStyleBackColor = true;
+            // 
+            // LogLbl
+            // 
+            this.LogLbl.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.LogLbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.LogLbl.Location = new System.Drawing.Point(0, 0);
+            this.LogLbl.Name = "LogLbl";
+            this.LogLbl.Size = new System.Drawing.Size(454, 487);
+            this.LogLbl.TabIndex = 19;
+            this.LogLbl.Text = "Log...";
+            // 
+            // ActionTableCnt
+            // 
+            this.ActionTableCnt.ColumnCount = 2;
+            this.ActionTableCnt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ActionTableCnt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ActionTableCnt.Controls.Add(this.RandomBtn, 0, 0);
+            this.ActionTableCnt.Controls.Add(this.DeleteBtn, 1, 0);
+            this.ActionTableCnt.Controls.Add(this.ReOpenBtn, 1, 1);
+            this.ActionTableCnt.Controls.Add(this.FindTargetBtn, 0, 1);
+            this.ActionTableCnt.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.ActionTableCnt.Location = new System.Drawing.Point(0, 282);
+            this.ActionTableCnt.Name = "ActionTableCnt";
+            this.ActionTableCnt.RowCount = 2;
+            this.ActionTableCnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ActionTableCnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.ActionTableCnt.Size = new System.Drawing.Size(558, 205);
+            this.ActionTableCnt.TabIndex = 19;
+            // 
+            // BrowseTableCnt
+            // 
+            this.BrowseTableCnt.ColumnCount = 1;
+            this.BrowseTableCnt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.BrowseTableCnt.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.BrowseTableCnt.Controls.Add(this.PathLbl, 0, 0);
+            this.BrowseTableCnt.Controls.Add(this.BrowseBtn, 0, 1);
+            this.BrowseTableCnt.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BrowseTableCnt.Location = new System.Drawing.Point(0, 135);
+            this.BrowseTableCnt.Name = "BrowseTableCnt";
+            this.BrowseTableCnt.RowCount = 2;
+            this.BrowseTableCnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.BrowseTableCnt.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.BrowseTableCnt.Size = new System.Drawing.Size(558, 147);
+            this.BrowseTableCnt.TabIndex = 20;
+            // 
+            // PathLbl
+            // 
+            this.PathLbl.AutoEllipsis = true;
+            this.PathLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PathLbl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PathLbl.Location = new System.Drawing.Point(3, 0);
+            this.PathLbl.Name = "PathLbl";
+            this.PathLbl.Size = new System.Drawing.Size(552, 73);
+            this.PathLbl.TabIndex = 19;
+            this.PathLbl.Text = "C:/Example";
             // 
             // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(711, 360);
-            this.Controls.Add(this.BrowseBtn);
-            this.Controls.Add(this.HeaderLbl);
-            this.Controls.Add(this.RandomBtn);
-            this.Controls.Add(this.PathLbl);
+            this.ClientSize = new System.Drawing.Size(1016, 487);
+            this.Controls.Add(this.ParentSplitContainer);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Window";
             this.Text = "Random File Opener";
+            this.ParentSplitContainer.Panel1.ResumeLayout(false);
+            this.ParentSplitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ParentSplitContainer)).EndInit();
+            this.ParentSplitContainer.ResumeLayout(false);
+            this.ActionTableCnt.ResumeLayout(false);
+            this.BrowseTableCnt.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Label PathLbl;
-        private System.Windows.Forms.Button RandomBtn;
-        private System.Windows.Forms.Label HeaderLbl;
+
+        private System.Windows.Forms.SplitContainer ParentSplitContainer;
+        private System.Windows.Forms.Button FindTargetBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Button ReOpenBtn;
         private System.Windows.Forms.Button BrowseBtn;
+        private System.Windows.Forms.Label HeaderLbl;
+        private System.Windows.Forms.Button RandomBtn;
+        private System.Windows.Forms.Label LogLbl;
+        private System.Windows.Forms.TableLayoutPanel ActionTableCnt;
+        private System.Windows.Forms.TableLayoutPanel BrowseTableCnt;
+        private System.Windows.Forms.Label PathLbl;
     }
 }
 
--- RandomFileOpener/View/Window.cs
+++ RandomFileOpener/View/Window.cs
@@ -1,11 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace RandomFileOpener
@@ -13,12 +5,6 @@ namespace RandomFileOpener
     public partial class Window:Form
     {
         public Window() => this.InitializeComponent();
-
-        private void LinkLabel1_LinkClicked(object sender,LinkLabelLinkClickedEventArgs e) =>
-            // Click on the link below to continue learning how to build a desktop app using WinForms!
-            System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
-
-        private void Button1_Click(object sender,EventArgs e) => MessageBox.Show("Thanks!");
-
+        //System.Diagnostics.Process.Start("http://aka.ms/dotnet-get-started-desktop");
     }
 }
